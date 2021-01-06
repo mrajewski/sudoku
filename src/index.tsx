fe4222c69d611d060e9d327b8c7f511004d3cbf9
Maciej Rajewski <rajewski.ma@gmail.com>
2021-01-06 20:45:18 +0100

    added styles to sudoku grid

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
+import { Content, Title, Grid, Card } from './components'
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>Hello World</div>
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+      <Card data-cy="card">
+        <Grid />
+      </Card>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root') as HTMLElement
 )
