055a2b4be12443cdd6f90404bdc8c4716fa223a5
Maciej Rajewski <rajewski.ma@gmail.com>
2021-01-06 22:09:06 +0100

    redux init

---
+++ src/utils/create-full-grid/index.ts
@@ -0,0 +1,20 @@
+import { fillGrid } from 'utils'
+import { GRID } from 'typings'
+
+function createFullGrid(): GRID {
+  const grid: GRID = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]
+  fillGrid(grid)
+  return grid
+}
+
+export default createFullGrid
